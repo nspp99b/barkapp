4d90876ffd3ea4776c7efb27918d7666a4ff8a95
nspp99b <chromepanthers@gmail.com>
2018-01-02 16:40:48 -0500

    added user routes, controller, actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help' #(help_path)
   get '/about', to: 'static_pages#about' #(about_path)
   get '/contact', to: 'static_pages#contact' #(contact_path)
-  
+
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
