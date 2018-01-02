c8de6677845322c20308793544e42471f10f420d
nspp99b <chromepanthers@gmail.com>
2018-01-02 17:40:56 -0500

    added session routes, session controller, defined session helper methods, fixed bootstrap dropdown

--- Gemfile
+++ Gemfile
@@ -7,9 +7,10 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.4'
+gem 'rails'
 # Use sqlite3 as the database for Active Record
 gem 'bootstrap-sass'
+# gem 'jquery-rails'
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help' #(help_path)
   get '/about', to: 'static_pages#about' #(about_path)
   get '/contact', to: 'static_pages#contact' #(contact_path)
+  get '/signup', to: 'users#new'  #a signup is really creating a user object
+  delete '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new' #sessions controller handles logging
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 
-
-  resources :users
+  resources :users #maybe clean up new and create routes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
