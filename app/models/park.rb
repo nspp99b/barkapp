2d6d958360fee03eaa0d915da7d32f79470fa8cf
nspp99b <chromepanthers@gmail.com>
2018-01-04 16:45:32 -0500

    edited index page, index controller action, parks model to display top threes

--- app/models/park.rb
+++ app/models/park.rb
@@ -23,10 +23,20 @@ class Park < ApplicationRecord
     self.reviews.average(:scene)
   end
 
-  def self.sorted_by_average
-    parks = self.all
-    parks.sort {|a, b| b.reviews.average(:overall) <=> a.reviews.average(:overall)}
+  def self.sorted_by_overall_average
+    self.all.sort {|a, b| b.overall_average <=> a.overall_average}
   end
 
+  def self.sorted_by_energy_level_average
+    self.all.sort {|a, b| b.energy_level_average <=> a.energy_level_average}
+  end
+
+  def self.sorted_by_facilities_average
+    self.all.sort {|a, b| b.facilities_average <=> a.facilities_average}
+  end
+
+  def self.sorted_by_scene_average
+    self.all.sort {|a, b| b.scene_average <=> a.scene_average}
+  end
 
 end
